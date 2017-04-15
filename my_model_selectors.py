4b2eef9ab24b7aa281873fe25405dd7b47d3cd09
Ren Silva <ren.silva@catchun.com>
2017-04-15 21:54:17 +1000

    updated notebook

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -125,5 +125,8 @@ class SelectorCV(ModelSelector):
         # raise NotImplementedError
 
 
-        best_num_components = self.n_constant
-        return self.base_model(best_num_components)
+        word_sequences = training.get_word_sequences(self.this_word)
+        split_method = KFold()
+        for cv_train_idx, cv_test_idx in split_method.split(word_sequences):
+            print("Train fold indices:{} Test fold indices:{}".format(cv_train_idx, cv_test_idx))  
+            
